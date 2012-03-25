54a7e5114701c4147bc0d01abed5b2735ab5d5de
toshihide tamura <tamura33363@gmail.com>
2012-03-25 22:36:08 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kashikari::Application.config.secret_token = '53918306ebfe7dcd6857b51c77a9db6f9fed859753f32becd277f43c1303e94c90fb7f7a8b21bad5a706f2bd5f2b94bd544c0f7134d868e7127e25f238b56c6a'
